be4a0fa82711e71cadaed022f6258b1432678eda
lisdanoviati <lisdanoviati36@gmail.com>
2026-05-14 15:45:31 +0700

    tes

--- resources/views/karyawan/dashboard.blade.php
+++ resources/views/karyawan/dashboard.blade.php
@@ -94,7 +94,7 @@
           viewBox="0 0 24 24" 
           fill="none" 
           stroke="#C8966C" 
-          stroke-width="2.2"
+          stroke-width="2.2" 
           class="group-hover:stroke-white transition"
         >
           <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"/>
