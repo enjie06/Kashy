1a1237da65fb36839290f940df2305c1bf967fc9
Miranda-Nggln <mirandanainggolan568@gmail.com>
2026-05-23 21:23:51 +0700

    p

--- routes/web.php
+++ routes/web.php
@@ -65,9 +65,13 @@ Route::middleware(['auth'])->group(function () {
         return view('kasir.shiftkasir');
     })->name('kasir.shiftkasir');
 
-Route::get('/kasir/profil', function () {
-    return view('kasir.profil');
-})->middleware('auth')->name('kasir.profil');
+    Route::get('/kasir/profil', [ProfileController::class, 'kasirProfile'])
+    ->middleware('auth')
+    ->name('kasir.profil');
+
+    Route::get('/kasir/profil', [ProfileController::class, 'kasirProfile'])
+    ->middleware('auth')
+    ->name('kasir.profil');
 
     Route::get('/kasir/riwayattransaksi', function () {
         return view('kasir.riwayattransaksi');
